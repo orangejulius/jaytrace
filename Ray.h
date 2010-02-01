5d93972470473da6291f1ccd76d16c6c6118d08c
Julian Simioni <julian.simioni@gmail.com>
2010-01-31 20:26:17 -0800

    Created ray class
    Created a class Ray that abstracts the handling of the basic ray
    calculations.

---
+++ Ray.h
@@ -0,0 +1,33 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <Eigen/Core>
+
+using Eigen::Vector3d;
+using Eigen::Vector4d;
+using Eigen::Matrix4d;
+
+class Ray
+{
+protected:
+	Vector3d origin;
+	Vector3d direction;
+
+public:
+	Ray();
+	Ray(Vector3d origin, Vector3d direction);
+
+	Vector3d getOrigin() {
+		return origin;
+	}
+	Vector3d getDirection() {
+		return direction;
+	}
+
+	Ray getTransformedRay(Matrix4d transformationMatrix);
+	Vector3d getPosition(double time);
+
+	bool operator==(const Ray& r) const;
+};
+
+#endif // RAY_H

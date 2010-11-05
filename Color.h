c7b03a6b12018f899028aca54124a4c7c262e6b9
Julian Simioni <julian.simioni@gmail.com>
2010-11-04 23:30:35 -0700

    Add Color class

---
+++ Color.h
@@ -0,0 +1,35 @@
+#ifndef COLOR_H_INCLUDED
+#define COLOR_H_INCLUDED
+
+class Color
+{
+public:
+	Color();
+	Color(float r, float g, float b, float a = 1);
+
+	float getRed() const;
+	void setRed(float r);
+
+	float getGreen() const;
+	void setGreen(float g);
+
+	float getBlue() const;
+	void setBlue(float b);
+
+	Color operator +(const Color &c) const;
+	Color operator -(const Color &c) const;
+	Color& operator +=(const Color &c);
+	Color operator *(float d) const;
+	Color operator *(Color c) const;
+
+	bool operator ==(const Color &c) const;
+	bool operator !=(const Color &c) const;
+
+private:
+	float red;
+	float green;
+	float blue;
+	float alpha;
+};
+
+#endif // COLOR_H_INCLUDED

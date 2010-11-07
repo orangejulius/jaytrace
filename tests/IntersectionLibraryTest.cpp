670244b4cf069a9ba00041b7eb4aedc6d223bc62
Julian Simioni <julian.simioni@gmail.com>
2010-11-07 13:11:06 -0800

    Add IntersectionLibrary occultation test
    Test that depth is handled properly when computing intersections and
    that the first intersection is returned regardless of the internal
    workings of the intersection library.

--- tests/IntersectionLibraryTest.cpp
+++ tests/IntersectionLibraryTest.cpp
@@ -36,3 +36,36 @@ void IntersectionLibraryTest::testIntersections()
 
 	QVERIFY(info3);
 }
+
+void IntersectionLibraryTest::testOccultation()
+{
+	IntersectionLibrary intersectionLibrary;
+
+	NodePointer t1(new TranslationNode(10,0,0));
+	NodePointer t2(new TranslationNode(10,0,0,t1));
+
+	//radius 1 spheres with centers at (10,0,0) and (20,0,0)
+	RayObjectPointer sphere1(new Sphere(t1));
+	RayObjectPointer sphere2(new Sphere(t2));
+
+	//vector starting at origin heading in the +x direction
+	Ray ray1(Vector3d(0,0,0),Vector3d(1,0,0));
+	//vector coming from the oposite direction
+	Ray ray2(Vector3d(30,0,0),Vector3d(-1,0,0));
+
+	intersectionLibrary.addObject(sphere1);
+	intersectionLibrary.addObject(sphere2);
+
+	IntersectionInfo* info1 = intersectionLibrary.intersect(ray1);
+	IntersectionInfo* info2 = intersectionLibrary.intersect(ray2);
+
+	QVERIFY(info1);
+	QCOMPARE(info1->normal,Vector3d(-1,0,0));
+	//correct intersection is at time 9 when the ray hits the first sphere
+	QCOMPARE(info1->time, 9.0);
+
+	QVERIFY(info2);
+	QCOMPARE(info2->normal,Vector3d(1,0,0));
+	//correct intersection is at time 9 when the ray hits the second sphere
+	QCOMPARE(info2->time, 9.0);
+}

900687051b268529699560853745254199cfe8a8
Julian Simioni <julian.simioni@gmail.com>
2010-11-10 23:13:26 -0800

    Remove unneded Eigen constructor calls

--- ScalingNode.cpp
+++ ScalingNode.cpp
@@ -1,6 +1,6 @@
 #include "ScalingNode.h"
 
-ScalingNode::ScalingNode(double x, double y, double z, NodePointer parent): TransformNode(parent), scaling(Scaling3d(x, y, z))
+ScalingNode::ScalingNode(double x, double y, double z, NodePointer parent): TransformNode(parent), scaling(x, y, z)
 {
 	transform *= scaling;
 }

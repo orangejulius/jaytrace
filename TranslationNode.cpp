9c363cbdfd0a3794ec60aa7f716935ee136ca84a
Julian Simioni <julian.simioni@gmail.com>
2010-11-10 23:13:26 -0800

    Remove unneded Eigen constructor calls

--- TranslationNode.cpp
+++ TranslationNode.cpp
@@ -1,6 +1,6 @@
 #include "TranslationNode.h"
 
-TranslationNode::TranslationNode(double x, double y, double z, NodePointer parent): TransformNode(parent), translation(Translation3d(x,y,z))
+TranslationNode::TranslationNode(double x, double y, double z, NodePointer parent): TransformNode(parent), translation(x,y,z)
 {
 	transform *= translation;
 }

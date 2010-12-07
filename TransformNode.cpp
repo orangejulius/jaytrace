486571ce1c8d265567de3a3707e1a499e8a824cf
Julian Simioni <julian.simioni@gmail.com>
2010-12-06 23:26:26 -0800

    Support arbitrary transformations in TransformNode
    Allow the TransformNode class to store an arbitrary transformation,
    while still supporting subclasses for specific transformations.

--- TransformNode.cpp
+++ TransformNode.cpp
@@ -11,6 +11,14 @@ TransformNode::TransformNode(NodePointer parent): Node(parent)
 	inverseMatrix = transform.matrix().inverse();
 }
 
+TransformNode::TransformNode(Transform3d transform, NodePointer parent): Node(parent), transform(transform)
+{
+	if (parent) {
+		transform = parent->getMatrixState();
+	}
+	inverseMatrix = transform.matrix().inverse();
+}
+
 Transform3d TransformNode::getMatrixState()
 {
 	return transform;
--- TransformNode.h
+++ TransformNode.h
@@ -8,9 +8,10 @@ using Eigen::Transform3d;
 /* Class TransformNode
  *
  * This class handles the storage of the current matrix state.
- * It does not actually represent a generic transformation, which is currently unsupported.
- * Instead this class stores a Transform3d object for the transformation matrix that any child
- * nodes would use for rendering. Child nodes can store a reference to this matrix data and will
+ * It can store an arbitrary transformation matrix, but is primarily used by subclassing it and
+ * creating a cleaner interface around a specific transformation (rotation, translation, etc)
+ *
+ * Child nodes can store a reference to this matrix data and will
  * be told when the reference should be updated. In this way there is one precomputed source for
  * the transformation matrix that is always up to date and can be accessed by any node that is
  * transformed by it.
@@ -20,6 +21,9 @@ class TransformNode : public Node
 {
 public:
 	TransformNode(NodePointer parent = NodePointer(0));
+
+	/// Store an arbitrary transformation
+	TransformNode(Transform3d transform, NodePointer parent = NodePointer(0));
 
 	virtual Transform3d getMatrixState();
 

472c209c5a9a4955a9733742543b680d89f60b52
Julian Simioni <julian.simioni@gmail.com>
2010-12-06 23:28:36 -0800

    Add Camera class
    Add a basic Camera class that can take eye, look and up vectors and
    return a transformation matrix to convert world coordinates into camera
    coordinates.

---
+++ Camera.cpp
@@ -0,0 +1,53 @@
+#include "Camera.h"
+
+#include <Eigen/Array>
+
+using Eigen::Matrix4d;
+
+Camera::Camera()
+{
+	eye = Vector3d::Zero();
+	look = -Vector3d::UnitZ();
+	up = Vector3d::UnitY();
+}
+
+Camera::Camera(Vector3d eye, Vector3d look, Vector3d up): eye(eye), look(look), up(up)
+{
+
+}
+
+void Camera::setEye(Vector3d newEye)
+{
+	eye = newEye;
+}
+
+void Camera::setLook(Vector3d newLook)
+{
+	look = newLook;
+}
+
+void Camera::setUp(Vector3d newUp)
+{
+	up = newUp;
+}
+
+Transform3d Camera::getTransform() const
+{
+	//first calculate the camera coordinate system
+	Vector3d n = eye - look;
+	Vector3d u = up.cross(n);
+
+	n.normalize();
+	u.normalize();
+
+	Vector3d v = n.cross(u);
+
+	//create a transformation matrix to convert world coordinates into camera coordinates
+	Matrix4d m = Matrix4d::Identity();
+	m<<u.x(), u.y(), u.z(), -eye.dot(u),
+	   v.x(), v.y(), v.z(), -eye.dot(v),
+	   n.x(), n.y(), n.z(), -eye.dot(n),
+	   0, 0, 0, 1;
+
+	return Transform3d(m);
+}

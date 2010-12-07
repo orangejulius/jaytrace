112248838a8193ffbfd500ee66ea2e69080d4086
Julian Simioni <julian.simioni@gmail.com>
2010-12-06 23:28:36 -0800

    Add Camera class
    Add a basic Camera class that can take eye, look and up vectors and
    return a transformation matrix to convert world coordinates into camera
    coordinates.

---
+++ Camera.h
@@ -0,0 +1,35 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+using Eigen::Vector3d;
+using Eigen::Transform3d;
+
+class Camera
+{
+public:
+	Camera();
+	Camera(Vector3d eye, Vector3d look, Vector3d up);
+
+	void setEye(Vector3d eye);
+	void setLook(Vector3d look);
+	void setUp(Vector3d up);
+
+	/// Return a transformation to convert world coordinates into camera coordinates
+	Transform3d getTransform() const;
+
+protected:
+
+	/// The location of the camera in world coordinates
+	Vector3d eye;
+
+	/// A point along the view direction from the camera in world coordinates
+	Vector3d look;
+
+	/// A vector in world coordinates that determines the top of the viewplane
+	Vector3d up;
+};
+
+#endif // CAMERA_H

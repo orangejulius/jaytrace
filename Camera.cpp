e52072e08f629aca9c7f5074e0bf13cc4e605698
Julian Simioni <julian.simioni@gmail.com>
2010-12-15 00:08:57 -0800

    Store camera coordinate axes in Camera class

--- Camera.cpp
+++ Camera.cpp
@@ -9,39 +9,38 @@ Camera::Camera()
 	eye = Vector3d::Zero();
 	look = -Vector3d::UnitZ();
 	up = Vector3d::UnitY();
+
+	computeCameraCoordinates();
 }
 
 Camera::Camera(Vector3d eye, Vector3d look, Vector3d up): eye(eye), look(look), up(up)
 {
-
+	computeCameraCoordinates();
 }
 
 void Camera::setEye(Vector3d newEye)
 {
 	eye = newEye;
+
+	computeCameraCoordinates();
 }
 
 void Camera::setLook(Vector3d newLook)
 {
 	look = newLook;
+
+	computeCameraCoordinates();
 }
 
 void Camera::setUp(Vector3d newUp)
 {
 	up = newUp;
+
+	computeCameraCoordinates();
 }
 
 Transform3d Camera::getTransform() const
 {
-	//first calculate the camera coordinate system
-	Vector3d n = eye - look;
-	Vector3d u = up.cross(n);
-
-	n.normalize();
-	u.normalize();
-
-	Vector3d v = n.cross(u);
-
 	//create a transformation matrix to convert world coordinates into camera coordinates
 	Matrix4d m = Matrix4d::Identity();
 	m << u.x(), u.y(), u.z(), -eye.dot(u),
@@ -50,4 +49,15 @@ Transform3d Camera::getTransform() const
 	  0, 0, 0, 1;
 
 	return Transform3d(m);
+}
+
+void Camera::computeCameraCoordinates()
+{
+	n = eye - look;
+	u = up.cross(n);
+
+	n.normalize();
+	u.normalize();
+
+	v = n.cross(u);
 }

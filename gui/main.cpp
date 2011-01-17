15480359b2ed702bc371382a539d408d4561bbe7
Julian Simioni <julian.simioni@gmail.com>
2011-01-17 12:30:02 -0800

    Add an empty MainWindow
    Create a simple MainWindow class that simply displays an empty window.

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,4 +1,11 @@
+#include <QApplication>
+
+#include "MainWindow.h"
+
 int main(int argc, char* argv[])
 {
-	return 0;
+	QApplication app(argc, argv);
+	MainWindow window;
+	window.show();
+	return app.exec();
 }

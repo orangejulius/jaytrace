89c68d423a803296e5ebbd426ea5839fdb8cf550
Julian Simioni <julian.simioni@gmail.com>
2011-02-09 21:20:59 -0800

    Add camera to 4 sphere demo scene

--- Scene.cpp
+++ Scene.cpp
@@ -107,6 +107,15 @@ Scene Scene::getSpiralScene()
 Scene Scene::get4SphereScene()
 {
 	Scene scene;
+
+	Vector3d eye(Vector3d(0, 0, 0));
+	Vector3d look(Vector3d(0, 0, -100));
+	Vector3d up(Vector3d(0, 1, 0));
+	Camera camera(eye, look, up);
+	scene.setCamera(camera);
+
+	NodePointer rootNode(new TransformNode(camera.getTransform()));
+
 	LightPointer l1(new Light(Vector3d(-3,12,-15),0));
 	Color white(1,1,1);
 	l1->setAmbient(white);
@@ -118,7 +127,7 @@ Scene Scene::get4SphereScene()
 	l2->setDiffuse(purple);
 	l2->setSpecular(purple);
 
-	NodePointer t1(new TranslationNode(0,3,-20));
+	NodePointer t1(new TranslationNode(0,3,-20, rootNode));
 	MaterialNodePointer m1(new MaterialNode(t1));
 	Color m1Color(.5,.2,.2);
 	m1->setAmbient(m1Color*.25);
@@ -127,7 +136,7 @@ Scene Scene::get4SphereScene()
 	m1->setShininess(10);
 	SceneObjectPointer s1(new Sphere(m1));
 
-	NodePointer t2(new TranslationNode(0,0,-20));
+	NodePointer t2(new TranslationNode(0,0,-20, rootNode));
 	NodePointer sc1(new ScalingNode(2,2,2,t2));
 	MaterialNodePointer m2(new MaterialNode(sc1));
 	Color m2Color(.7,.7,.7);
@@ -137,7 +146,7 @@ Scene Scene::get4SphereScene()
 	m2->setShininess(20);
 	SceneObjectPointer s2(new Sphere(m2));
 
-	NodePointer t3(new TranslationNode(3,0,-20));
+	NodePointer t3(new TranslationNode(3,0,-20, rootNode));
 	MaterialNodePointer m3(new MaterialNode(t3));
 	Color m3Color(.3,.5,.3);
 	m3->setAmbient(m3Color*.25);
@@ -146,7 +155,7 @@ Scene Scene::get4SphereScene()
 	m3->setShininess(30);
 	SceneObjectPointer s3(new Sphere(m3));
 
-	NodePointer t4(new TranslationNode(-3,0,-20));
+	NodePointer t4(new TranslationNode(-3,0,-20, rootNode));
 	MaterialNodePointer m4(new MaterialNode(t4));
 	Color m4Color(.3,.3,.7);
 	m4->setAmbient(m4Color*.25);
@@ -155,7 +164,7 @@ Scene Scene::get4SphereScene()
 	m4->setShininess(30);
 	SceneObjectPointer s4(new Sphere(m4));
 
-	NodePointer t5(new TranslationNode(0,-3,-20));
+	NodePointer t5(new TranslationNode(0,-3,-20, rootNode));
 	MaterialNodePointer m5(new MaterialNode(t5));
 	Color m5Color(.5,.2,.5);
 	m5->setAmbient(m5Color*.25);

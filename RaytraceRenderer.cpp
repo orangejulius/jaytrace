ea9cd5b568b65924814f48734b04c68e3df5c9e3
Julian Simioni <julian.simioni@gmail.com>
2012-02-07 23:05:30 -0800

    Use openmp to parallelize the main raytracer loop

--- RaytraceRenderer.cpp
+++ RaytraceRenderer.cpp
@@ -37,6 +37,7 @@ QImage RaytraceRenderer::render()
 
 	image.fill(0xff000000);
 
+	#pragma omp parallel for num_threads(4)
 	for (unsigned int row = 0; row < height; row++) {
 		for (unsigned int col = 0; col < width; col++) {
 			Color pixelColor(0, 0, 0);

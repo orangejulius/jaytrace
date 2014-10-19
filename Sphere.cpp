070cffd32b8328974df1ac679006201b55979cb0
Julian Simioni <julian.simioni@gmail.com>
2014-10-18 20:26:08 -0700

    Remove superfluous call to this

--- Sphere.cpp
+++ Sphere.cpp
@@ -18,7 +18,7 @@ Sphere::~Sphere()
 
 IntersectionInfo* Sphere::intersect(const Ray& ray)
 {
-	Affine3d transform = this->getMatrixState();
+	Affine3d transform = getMatrixState();
 
 	if (transform.matrix().determinant() == 0) {
 		qDebug() << "Matrix not invertible!";

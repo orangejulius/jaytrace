5c751be8fe4bad192d787011890f739c85fe2717
Julian Simioni <julian.simioni@gmail.com>
2014-10-18 21:12:03 -0700

    Use automatic variables in tests

--- tests/SphereTest.cpp
+++ tests/SphereTest.cpp
@@ -10,9 +10,9 @@ void SphereTest::testMultipleTransformParents()
 {
 	NodePointer t1(new TranslationNode(1, 0, 0));
 	NodePointer t2(new TranslationNode(0, 2, 0, t1));
-	QSharedPointer<Sphere> s1(new Sphere(t2));
+	Sphere s1(t2);
 
-	Affine3d actualTransform = s1->getTransform();
+	Affine3d actualTransform = s1.getTransform();
 	Affine3d expectedTransform(Translation3d(1, 2, 0));
 
 	QCOMPARE(expectedTransform.matrix(), actualTransform.matrix());
@@ -32,11 +32,11 @@ void SphereTest::testUntransformedIntersection()
 void SphereTest::testTransformedIntersection()
 {
 	NodePointer t1(new TranslationNode(0, 5, 0));
-	QSharedPointer<Sphere> s1(new Sphere(t1));
+	Sphere s1(t1);
 
 	Ray ray1(Vector3d(-10, 5, 0), Vector3d(10, 0, 0));
 	Ray ray2(Vector3d(-10, 0, 0), Vector3d(10, 0, 0));
 
-	QVERIFY(s1->intersect(ray1));
-	QVERIFY(!s1->intersect(ray2));
+	QVERIFY(s1.intersect(ray1));
+	QVERIFY(!s1.intersect(ray2));
 }

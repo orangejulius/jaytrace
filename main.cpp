439af81fe2175f86eef7a8ec0a294fcf3157dc57
Julian Simioni <julian@simioni.org>
2015-04-06 00:01:57 +0200

    Add command line parameters for rows, columns, antialiasing samples

--- main.cpp
+++ main.cpp
@@ -5,10 +5,22 @@
 #include <cmath>
 #include <QDate>
 
-int main()
+int main(int argc, char* argv[])
 {
+	unsigned int rows = 640;
+	unsigned int cols = 480;
+	unsigned int aaSamples = 1;
+
+	if (argc > 2) {
+			rows = atoi(argv[1]);
+			cols = atoi(argv[2]);
+	}
+
+	if (argc > 3) {
+		aaSamples = atoi(argv[3]);
+	}
 	ScenePointer scene(new OBJScene());
-	RaytraceRenderer raytraceRenderer(scene);
+	RaytraceRenderer raytraceRenderer(scene, rows, cols, aaSamples);
 
 	QImage output = raytraceRenderer.render();
 	QString filename = QDateTime::currentDateTime().toString(Qt::ISODate) + ".png";
